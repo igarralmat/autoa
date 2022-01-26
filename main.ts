e3c2c48499b369a3255004a65cab7099384f58dc
igarralmat <93262344+igarralmat@users.noreply.github.com>
2022-01-26 14:02:25 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
-basic.forever(function () {
+function Limpiaparabrisas () {
 	
+}
+function Ezkerreko_intermitentea () {
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+}
+function Eskuineko_intermitentea () {
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+}
+radio.setGroup(2)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        Eskuineko_intermitentea()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        Ezkerreko_intermitentea()
+    }
+    if (input.logoIsPressed()) {
+        Limpiaparabrisas()
+    }
 })
